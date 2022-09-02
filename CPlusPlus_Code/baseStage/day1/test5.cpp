670ac0cd88520c3f6edcf53c5ce69586fa525e71
wangsomebody <1605255735@qq.com>
2022-09-02 23:17:05 +0800

    布尔类型定义

---
+++ CPlusPlus_Code/baseStage/day1/test5.cpp
@@ -0,0 +1,16 @@
+// 布尔数据类型bool
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //定义布尔类型
+    bool flag = true;
+    cout << flag << endl;
+    flag = false;
+    cout << flag << endl;
+    //布尔类型的大小
+    cout << sizeof(bool) << endl;
+    system("pause");
+    return 0;
+}
