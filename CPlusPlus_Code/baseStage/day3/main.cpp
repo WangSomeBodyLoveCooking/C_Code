25804da9c5307db667171d2fbe6acb35cfb134ab
wangsomebody <1605255735@qq.com>
2023-03-12 20:07:27 +0800

    分文件编写

---
+++ CPlusPlus_Code/baseStage/day3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    int b = 3;
+    int c = add(a, b);
+    cout << c << endl;
+    system("pause");
+    return 0;
+}
---
+++ CPlusPlus_Code/baseStage/day3/test.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+
+int add(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
